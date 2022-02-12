1aebf005f6bf0ee563075eeb5d0a7dd7bee36ed0
celebrater <tkgm45nbchk@gmail.com>
2022-02-12 17:46:38 +0900

    give the React list.txt succeeded

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
 <?php
+
+ini_set( 'display_errors', 1 );
+error_reporting(E_ALL);
+
+define('NOVELS_DIR', __DIR__ . '/novels');
+
+$novel_id = isset($_GET["novel"]) ? (int)$_GET["novel"] : null;
+$json = [];
+
+if($novel_id === null) {
+    $novel = file(NOVELS_DIR . "/novels_list.txt");
+    $json = array_merge($json, $novel);
+} else {
+    $novel = file(NOVELS_DIR . "/novels_list.txt");
+    $line = explode("|", $novel[$novel_id]);
+    $dir = str_replace(["\n", "\r"], "", $line[1]);
+    $episodes = file(NOVELS_DIR . "/" . $dir . "/list.txt");
+    $json = array_merge($json, $episodes);
+}
+
+
+echo json_encode($json, JSON_UNESCAPED_UNICODE);
