130e9c60e4ad2cbe4e3b6a80d474a34ad3b766f7
celebrater <tkgm45nbchk@gmail.com>
2022-02-12 17:57:45 +0900

    give the React a test episode succeeded

--- index.php
+++ index.php
@@ -6,6 +6,9 @@ error_reporting(E_ALL);
 define('NOVELS_DIR', __DIR__ . '/novels');
 
 $novel_id = isset($_GET["novel"]) ? (int)$_GET["novel"] : null;
+$chap_id = isset($_GET["chap"]) ? (int)$_GET["chap"] : null;
+$ep_id = isset($_GET["ep"]) ? (int)$_GET["ep"] : null;
+
 $json = [];
 
 if($novel_id === null) {
@@ -14,10 +17,15 @@ if($novel_id === null) {
 } else {
     $novel = file(NOVELS_DIR . "/novels_list.txt");
     $line = explode("|", $novel[$novel_id]);
-    $dir = str_replace(["\n", "\r"], "", $line[1]);
-    $episodes = file(NOVELS_DIR . "/" . $dir . "/list.txt");
-    $json = array_merge($json, $episodes);
+    $dir = NOVELS_DIR . "/" . str_replace(["\n", "\r"], "", $line[1]);
+    $episodes = file($dir . "/list.txt");
+    if($ep_id === null){
+        $json = array_merge($json, $episodes);
+    } else {
+        $ep_line = explode("|", $episodes[$ep_id]);
+        $episode = file($dir . "/txts/" . $ep_line[1] . ".txt");
+        $json = array_merge($json, $episode);
+    }
 }
-
 
 echo json_encode($json, JSON_UNESCAPED_UNICODE);
